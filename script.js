45c7f477d38093251b9c341d18df84c0b875f73e
CodeWithRanajit <ranajitchattaraj2004@gmail.com>
2025-06-17 11:49:33 +0530

    Modify

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ let timer=document.getElementById('displaytime');
 let time=new Date();
 let now=time.toLocaleTimeString();
 timer.innerText=now;
-timer.style.fontSize="45px";
-timer.style.paddingLeft="70px";
+timer.style.fontSize="55px";
+timer.style.paddingLeft="1px";
 
 let fun=()=>{
 let timer=document.getElementById('displaytime');
 let time=new Date();
 let now=time.toLocaleTimeString();
 timer.innerText=now;
-timer.style.fontSize="45px";
-timer.style.paddingLeft="70px";
 };
 setInterval(fun,1000);
 
